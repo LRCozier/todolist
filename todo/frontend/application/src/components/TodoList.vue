00eb4d2816384808bed0a1711183b50e7851fcbb
LRCozier <lrcozier@gmail.com>
2025-08-07 21:42:11 +0100

    created initial components

---
+++ todo/frontend/application/src/components/TodoList.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="todo-list">
+    <Todoitem
+    v-for="task in tasks"
+    :key="'task.id'"
+    :task="task"
+    @toggle="toggleTask"
+    @edit="editTask"
+    @delete="deleteTask" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, DefineComponent, PropType } from 'vue';
+import {Task} from '@/types/task';
+import Todoitem from './Todoitem.vue';
+
+export default defineComponent({
+  name: 'TodoList',
+  components: {Todoitem},
+  props: {
+    task: {
+      type: Array as PropType<Task[]>,
+      required: true
+    }
+  },
+  emits: ['toggle', 'edit', 'delete'],
+  methods: {
+    toggleTask(id:number) {
+      this.$emit('toggle', id);
+    },
+    editTask(task:Task) {
+      this.$emit('edit', task);
+    },
+    deleteTask(id:number) {
+      this.$emit('delete', id);
+    }
+  }
+})
+</script>
+
+<style>
+
+</style>
---
+++ todo/frontend/application/src/components/Todoitem.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="todo-item" :class="{ completed: task.completed}">
+    <input type="checkbox" :checked="task.completed" @change="$emit('toggle', task.id)" />
+    <span>{{ task.title }}</span>
+    <button @click="$emit('edit', task)"></button>
+    <button @click="$emit('delete', task.id)"></button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, DefineComponent, PropType } from 'vue';
+import {Task} from '@/types/task';
+
+export default defineComponent(
+  {
+    name: 'TodoItem',
+    props: {
+      task: {
+        type: Object as PropType<Task>,
+          required: true
+      }
+    },
+    emits: ['toggle', 'edit', 'delete']
+  }
+);
+</script>
+
+<style>
+
+</style>
