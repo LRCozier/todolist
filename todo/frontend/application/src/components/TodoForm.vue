4493131f2d4930a742e20334c6b40dfe4f79a66c
LRCozier <lrcozier@gmail.com>
2025-08-08 20:48:45 +0100

    created to do form component

---
+++ todo/frontend/application/src/components/TodoForm.vue
@@ -0,0 +1,22 @@
+<template>
+  <form
+  @submit.prevent="handleSubmit"
+  class="todo-form">
+  <input
+  type="text"
+  v-model="title"
+  placeholder="Title"
+  required />
+
+
+
+  </form>
+</template>
+
+<script lang="ts">
+
+</script>
+
+<style>
+
+</style>
