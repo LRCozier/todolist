12c23a502660c9e92069d81e14e15e61464bb54b
LRCozier <lrcozier@gmail.com>
2025-08-09 19:00:10 +0100

    added font awesome icons to main.ts

--- todo/frontend/application/src/main.ts
+++ todo/frontend/application/src/main.ts
@@ -2,4 +2,14 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faTrash } from '@fortawesome/pro-light-svg-icons';
+import { faPencil } from '@fortawesome/pro-light-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faTrash);
+library.add(faPencil);
+
 createApp(App).mount('#app')
+app.component('font-awesome-icon', FontAwesomeIcon);
+app.mount('#app');
