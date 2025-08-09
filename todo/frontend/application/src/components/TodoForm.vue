689fa26b7eead3861cc81e85d0e237fd7ad6032d
LRCozier <lrcozier@gmail.com>
2025-08-09 14:14:05 +0100

    completed TodoForm.vue

--- todo/frontend/application/src/components/TodoForm.vue
+++ todo/frontend/application/src/components/TodoForm.vue
@@ -7,14 +7,85 @@
   v-model="title"
   placeholder="Title"
   required />
-
-
-
+  <textarea 
+  v-model="description"
+  placeholder="Enter Task Here">
+  </textarea>
+  <div class="form-actions">
+    <button type="submit">
+      {{ submitTest }}
+    </button>
+    <button
+    v-if="showCancel"
+    type="button"
+    @click="handleCancel">
+    Cancel
+    </button>
+  </div>
   </form>
 </template>
 
 <script lang="ts">
+import { defineComponent, PropType, ref, watch } from 'vue';
+import { Task } from '../types/tasks';
 
+export default defineComponent({
+  name: 'TodoForm',
+  props: {
+    initialTask:{
+      type: Object as PropType<Partial<Task>>,
+      default: null,
+    },
+    submitText: {
+      type: String,
+      default: 'Add Task',
+    },
+    showCancel: {
+      type: Boolean,
+      default: false,
+    },
+  },
+
+  emits: ['submit', 'cancel', 'create'],
+  setup(props, {emit}) {
+    const title = ref('');
+    const description = ref('');
+
+    //populate form if editting an existing task
+    watch(() => props.initialTask, (task) = > {
+      if (task){
+        title.value = task.title || '';
+        description.value = task.description || '';
+      }
+    }, {immediate: true});
+
+    const handleSubmit = () => {
+      const taskData = {
+        title: title.value,
+        description: description.value,
+      };
+
+    if (props.initialTask?.id) {
+      emit('submit', taskData)
+    } else {
+      emit('create', taskData.title);
+    }
+
+    // reset form if not editting a task
+    if (!props.initialTask) {
+      title.value = '';
+      description.value = '';
+    }
+    };
+
+    const handleCancel = () => {
+      emit('cancel');
+    }
+
+    return { title, description, handleSubmit, handleCancel};
+
+  },
+});
 </script>
 
 <style>
