eb1eb9fb006c975146753f3c2f82e5cd02320d4f
LRCozier <lrcozier@gmail.com>
2025-08-13 17:46:51 +0100

    updated interfaces

--- todo/frontend/application/src/types/interfaces.ts
+++ todo/frontend/application/src/types/interfaces.ts
@@ -4,8 +4,19 @@ export interface Task {
   title: string;
   completed: boolean;
   description?: string;
-  createdAt?: string;
+  createdAt: string;
   updatedAt: string;
+}
+
+export interface TaskCreatePayload {
+  title: string;
+  description?: string;
+}
+
+export interface TaskUpdatePayload {
+  title?: string;
+  description?: string;
+  completed?: boolean;
 }
 
 export interface ApiError {
