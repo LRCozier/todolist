de8840553aa2dc961471608418c39fc7cb3c8126
LRCozier <lrcozier@gmail.com>
2025-09-16 11:16:12 +0100

    amended security issue in todoitem.vue

--- todo/frontend/application/src/components/Todoitem.vue
+++ todo/frontend/application/src/components/Todoitem.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="todo-item" :class="{ completed: task.completed }">
     <input type="checkbox" :checked="task.completed" @change="$emit('toggle', task.id)" />
-    <span>{{ task.title }}</span>
+    <span v-text="task.title"></span> <!--Amended to use text binding instead of interpolation-->
     <button @click="$emit('edit', task)"><PencilSquareIcon class="h-5 w-5" /></button>
     <button @click="$emit('delete', task.id)"><TrashIcon class="h-5 w-5 text-red-500" /></button>
   </div>
