85473eb6a2fd97e06f142dab4960ec7e986d53da
LRCozier <lrcozier@gmail.com>
2025-09-19 09:47:12 +0100

    updated database.php

--- todo/backend/config/database.php
+++ todo/backend/config/database.php
@@ -1,5 +1,10 @@
 <?php
 
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
 function getPDO(): PDO {
   static $pdo = null;
 
@@ -11,9 +16,9 @@ function getPDO(): PDO {
     $charset = "utf8mb4";
     $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
     $options = [
-      PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+      PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
       PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-      PDO::ATTR_EMULATE_PREPARES   => false, 
+      PDO::ATTR_EMULATE_PREPARES => false, 
     ];
 
     try {
